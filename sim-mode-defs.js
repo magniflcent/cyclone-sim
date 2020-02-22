2a43b1eb70e1d9a412da986f0838e2371ab3dd32
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-02-22 21:26:50 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -451,7 +451,7 @@ ENV_DEFS.defaults.moisture = {
     },
     modifiers: {
         polarMoisture: 0.43,
-        tropicalMoisture: 0.62,
+        tropicalMoisture: 0.67,
         mountainMoisture: 0.2
     },
     noiseChannels: [

55853359ea0e1efed30226a9a24608521c2b8edc
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-02 18:47:30 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -121,7 +121,7 @@ ENV_DEFS.defaults.LLSteering = {
         // Jetstream
         let j = u.field('jetstream');
         // Cosine curve from 0 at poleward side of map to 1 at equatorward side
-        let h = map(cos(map(y,0,HEIGHT,0,PI)),-1.25,-1.375,1,1,0);
+        let h = map(cos(map(y,0,HEIGHT,0,PI)),-1.375,1,1,0);
         // westerlies
         let west = constrain(pow(1-h+map(u.noise(0),0,1,-0.3,0.3)+map(j,0,HEIGHT,-0.3,0.3),2)*4,0,4);
         // ridging and trades

c55cd77bdf0890c1d821e1e1300b253629d80779
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-03 20:36:38 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -299,7 +299,7 @@ ENV_DEFS.defaults.SSTAnomaly = {
     mapFunc: (u,x,y,z)=>{
         let v = u.noise(0);
         v = v*2;
-        let i = v<1 ? -0.5 : 1;
+        let i = v<1 ? -0.5 : 0.5;
         v = 1-abs(1-v);
         if(v===0) v = 0.000001;
         v = log(v);
@@ -387,7 +387,7 @@ ENV_DEFS.defaults.SST = {
         offSeasonPolarTemp: -3,
         peakSeasonPolarTemp: 0,
         offSeasonTropicsTemp: 27,
-        peakSeasonTropicsTemp: 27.5
+        peakSeasonTropicsTemp: 28
     }
 };
 ENV_DEFS[SIM_MODE_NORMAL].SST = {};

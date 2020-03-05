de59b5c7eb4f08948c7c4a6954a1315f104d316c
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-05 19:21:53 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -13,7 +13,7 @@ const SPAWN_RULES = {};
 
 SPAWN_RULES[SIM_MODE_NORMAL] = function(b){
     if(random()<0.015*sq((seasonalSine(b.tick)+1)/2)) b.spawn(false); //tropics spawn area
-    if(random()<0.0075) b.spawn(false,{x:random(0,WIDTH),y:random(0.5*HEIGHT,0.6*HEIGHT),sType:'l'}); // spawn area for areas like the GoM
+    if(random()<0.0075) b.spawn(false,{x:random(0,WIDTH),y:random(0.6*HEIGHT,0.6*HEIGHT),sType:'l'}); // spawn area for areas like the GoM
     if(random()<0.01-0.002*seasonalSine(b.tick)) b.spawn(true);                 // extratropical cyclones
 };
 SPAWN_RULES[SIM_MODE_HYPER] = function(b){
@@ -305,7 +305,7 @@ ENV_DEFS.defaults.SSTAnomaly = {
         v = log(v);
         let r;
         if(u.modifiers.r!==undefined) r = u.modifiers.r;
-        else r = 3;
+        else r = 0;
         v = -r*v;
         v = v*i;
         if(u.modifiers.bigBlobBase!==undefined && v>u.modifiers.bigBlobExponentThreshold) v += pow(u.modifiers.bigBlobBase,v-u.modifiers.bigBlobExponentThreshold)-1;

4213bf2552dc340a15408492c439639473d03bcc
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-09 18:47:24 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -298,7 +298,7 @@ ENV_DEFS.defaults.SSTAnomaly = {
     mapFunc: (u,x,y,z)=>{
         let v = u.noise(0);
         v = v*2;
-        let i = v<1 ? -1 : 1;
+        let i = v<1 ? -0.5 : 0.5;
         v = 1-abs(1-v);
         if(v===0) v = 0.000001;
         v = log(v);

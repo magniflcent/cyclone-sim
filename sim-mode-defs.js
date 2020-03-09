e34154a5b74bc259ffc18c57262f670e904840cf
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-09 11:18:21 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -387,7 +387,7 @@ ENV_DEFS.defaults.SST = {
         offSeasonPolarTemp: -3,
         peakSeasonPolarTemp: 0,
         offSeasonTropicsTemp: 25,
-        peakSeasonTropicsTemp: 29
+        peakSeasonTropicsTemp: 28.5
     }
 };
 ENV_DEFS[SIM_MODE_NORMAL].SST = {};

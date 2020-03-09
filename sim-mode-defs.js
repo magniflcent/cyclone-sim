67796b8c9048f21c90578c7eadcb9a560347597a
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-09 10:48:08 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -12,8 +12,8 @@ const SIM_MODE_EXPERIMENTAL = 4;
 const SPAWN_RULES = {};
 
 SPAWN_RULES[SIM_MODE_NORMAL] = function(b){
-    if(random()<0.009*sq((seasonalSine(b.tick)+1)/2)) b.spawn(false); //tropics spawn area
-    if(random()<0.009) b.spawn(false,{x:random(0,WIDTH),y:random(0.6*HEIGHT,0.6*HEIGHT),sType:'l'}); // spawn area for areas like the GoM
+    if(random()<0.0075*sq((seasonalSine(b.tick)+1)/2)) b.spawn(false); //tropics spawn area
+    if(random()<0.0075) b.spawn(false,{x:random(0,WIDTH),y:random(0.6*HEIGHT,0.7*HEIGHT),sType:'l'}); // spawn area for areas like the GoM
     if(random()<0.01-0.002*seasonalSine(b.tick)) b.spawn(true);                 // extratropical cyclones
 };
 SPAWN_RULES[SIM_MODE_HYPER] = function(b){

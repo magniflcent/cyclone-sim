ffe8e52a09b9313e69a73846b37766a10125f3e1
MagicalDrizzle <49761211+MagicalDrizzle@users.noreply.github.com>
2020-03-24 01:51:49 +0700

    attempt to fix AoE and misc.

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -31,8 +31,23 @@ SPAWN_RULES[SIM_MODE_MEGABLOBS] = function(b){
 SPAWN_RULES[SIM_MODE_EXPERIMENTAL] = SPAWN_RULES[SIM_MODE_HYPER];
 
 SPAWN_RULES[SIM_MODE_WPAC] = function(b){
-    if(random()<0.005) b.spawn(false,{x:random(0.1*WIDTH,0.7*WIDTH),y:random(0.6*HEIGHT,0.8*HEIGHT),sType:'l'}); //tropics spawn area
-    if(random()<0.01-0.002*seasonalSine(b.tick)) b.spawn(true);                 // extratropical cyclones
+	if(random()<0.0032) b.spawn(false,{x:random(0.27*WIDTH,0.73*WIDTH),y:random(0.51*HEIGHT,0.76*HEIGHT),sType:'l'});
+	if(random()<0.00027) b.spawn(false,{x:random(0.667*WIDTH,0.8*WIDTH),y:random(0.445*HEIGHT,0.509*HEIGHT),sType:'l'});
+	if(random()<0.002) b.spawn(false,{x:random(0.3*WIDTH,0.73*WIDTH),y:random(0.761*HEIGHT,0.9*HEIGHT),sType:'l'});
+    if(random()<0.0007) b.spawn(false,{x:random(0.731*WIDTH,0.807*WIDTH),y:random(0.511*HEIGHT,0.787*HEIGHT),sType:'l'}); //tropics spawn area
+	if(random()<0.0005) b.spawn(false,{x:random(0.731*WIDTH,0.807*WIDTH),y:random(0.788*HEIGHT,0.9*HEIGHT),sType:'l'});
+    if(random()<0.00025) b.spawn(false,{x:random(0.808*WIDTH,0.98*WIDTH),y:random(0.55*HEIGHT,0.83*HEIGHT),sType:'l'});
+	if(random()<0.0015) b.spawn(false,{x:random(0.145*WIDTH,0.244*WIDTH),y:random(0.648*HEIGHT,0.84*HEIGHT),sType:'l'});
+	if(random()<0.0007) b.spawn(false,{x:random(0.145*WIDTH,0.244*WIDTH),y:random(0.841*HEIGHT,0.9*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.115*WIDTH,0.14*WIDTH),y:random(0.667*HEIGHT,0.695*HEIGHT),sType:'l'});
+	if(random()<0.0001) b.spawn(false,{x:random(0.115*WIDTH,0.14*WIDTH),y:random(0.842*HEIGHT,0.91*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.182*WIDTH,0.209*WIDTH),y:random(0.62*HEIGHT,0.649*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.21*WIDTH,0.239*WIDTH),y:random(0.602*HEIGHT,0.649*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.24*WIDTH,0.269*WIDTH),y:random(0.56*HEIGHT,0.63*HEIGHT),sType:'l'});
+	if(random()<0.00055) b.spawn(false,{x:random(0.245*WIDTH,0.269*WIDTH),y:random(0.631*HEIGHT,0.685*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.266*WIDTH,0.301*WIDTH),y:random(0.761*HEIGHT,0.852*HEIGHT),sType:'l'});
+	if(random()<0.00025) b.spawn(false,{x:random(0.245*WIDTH,0.265*WIDTH),y:random(0.761*HEIGHT,0.9*HEIGHT),sType:'l'});
+	if(random()<0.01-0.002*seasonalSine(b.tick)) b.spawn(true);                 // extratropical cyclones
 };
 
 // ---- Definitions of Environmental Fields ---- //
@@ -236,7 +251,7 @@ ENV_DEFS.defaults.ULSteering = {
 ENV_DEFS[SIM_MODE_NORMAL].ULSteering = {};
 ENV_DEFS[SIM_MODE_HYPER].ULSteering = {
     modifiers: {
-        hadleyUpperBound: 3
+        hadleyUpperBound: 3.5
     }
 };
 ENV_DEFS[SIM_MODE_WILD].ULSteering = {
@@ -280,7 +295,7 @@ ENV_DEFS[SIM_MODE_MEGABLOBS].ULSteering = {};
 ENV_DEFS[SIM_MODE_EXPERIMENTAL].ULSteering = {};
 ENV_DEFS[SIM_MODE_WPAC].ULSteering = {
     modifiers: {
-        hadleyUpperBound: 3
+        hadleyUpperBound: 4.5
     }
 };
 

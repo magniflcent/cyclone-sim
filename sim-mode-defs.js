8df4c54d66ad4a3bdb082b2ebbb05014c128f580
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-26 16:19:35 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -385,7 +385,7 @@ ENV_DEFS.defaults.SST = {
         if(y<0) return 0;
         let anom = u.field('SSTAnomaly');
         let s = seasonalSine(z);
-        let w = map(cos(map(x,0,WIDTH,0,PI)),-0.9,0.9,0.9,0.9);
+        let w = map(cos(map(x,0,WIDTH,0,PI)),-1,1,1,1;
         let h0 = y/HEIGHT;
         let h1 = (sqrt(h0)+h0)/2;
         let h2 = sqrt(sqrt(h0));

c12a8b80222ceb9b9bd7a206adb57bafbbb47260
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-27 20:30:44 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -130,7 +130,7 @@ ENV_DEFS.defaults.LLSteering = {
         // Cosine curve from 0 at poleward side of map to 1 at equatorward side
         let h = map(cos(map(y,0,HEIGHT,0,PI)),-1.25,0.75,0.75,0);
         // westerlies
-        let west = constrain(pow(0.25-h+map(u.noise(0),0,1,-0.3,0.3)+map(j,0,HEIGHT,-0.3,0.3),2)*4,0,4);
+        let west = constrain(pow(0.5-h+map(u.noise(0),0,1,-0.3,0.3)+map(j,0,HEIGHT,-0.3,0.3),2)*4,0,4);
         // ridging and trades
         let ridging = constrain(u.noise(1)+map(j,0,HEIGHT,0.3,-0.3),0,1);
         let trades = constrain(pow(0.25+h+map(ridging,0,1,-0.3,0.3),2)*3,0,3);

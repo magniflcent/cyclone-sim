99ad90470812e462c17d00cbbccfe0b248cf3394
ProjectBluebell <58730826+ProjectBluebell@users.noreply.github.com>
2020-03-28 12:00:47 +0300

    Update sim-mode-defs.js

--- sim-mode-defs.js
+++ sim-mode-defs.js
@@ -13,7 +13,7 @@ const SIM_MODE_WPAC = 5;
 const SPAWN_RULES = {};
 
 SPAWN_RULES[SIM_MODE_NORMAL] = function(b){
-    if(random()<(0.007*sq((seasonalSine(b.tick)+1)/2)+0.002)) b.spawn(false,{x:random(0.025*WIDTH,0.7*WIDTH),y:random(0.6*HEIGHT,0.9*HEIGHT),sType:'l'}); //tropics spawn area
+    if(random()<(0.007*sq((seasonalSine(b.tick)+1)/2)+0.002)) b.spawn(false,{x:random(0.0*WIDTH,0.7*WIDTH),y:random(0.6*HEIGHT,0.9*HEIGHT),sType:'l'}); //tropics spawn area
     if(random()<0.01-0.002*seasonalSine(b.tick)) b.spawn(true);                 // extratropical cyclones
 };
 SPAWN_RULES[SIM_MODE_HYPER] = function(b){
@@ -130,10 +130,10 @@ ENV_DEFS.defaults.LLSteering = {
         // Cosine curve from 0 at poleward side of map to 1 at equatorward side
         let h = map(cos(map(y,0,HEIGHT,0,PI)),-1.25,0.75,0.75,0);
         // westerlies
-        let west = constrain(pow(0.5-h+map(u.noise(0),0,1,-0.3,0.3)+map(j,0,HEIGHT,-0.3,0.3),2)*4,0,4);
+        let west = constrain(pow(1-h+map(u.noise(0),0,1,-0.3,0.3)+map(j,0,HEIGHT,-0.3,0.3),2)*4,0,4);
         // ridging and trades
         let ridging = constrain(u.noise(1)+map(j,0,HEIGHT,0.3,-0.3),0,1);
-        let trades = constrain(pow(0.25+h+map(ridging,0,1,-0.3,0.3),2)*3,0,3);
+        let trades = constrain(pow(0.5+h+map(ridging,0,1,-0.3,0.3),2)*3,0,3);
         let tAngle = map(h,0.9,1,511*PI/512,15.75*PI/16); // trades angle
         // noise angle
         let a = map(u.noise(3),0,1,0,4*TAU);
